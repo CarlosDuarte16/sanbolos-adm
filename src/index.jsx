79c1a531fc5ac44ddb2b0c1780692b57494af30c
CarlosDuarte <ra52208844840@acaonsfatima.org.br>
2024-10-22 18:22:11 -0300

    Só pra atualizar

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,8 @@ import Login from './pages/page-login';
 import AddProduct from './pages/page-add-product';
 import EditProduct from './pages/page-edit-product';
 import Products from './pages/page-product';
+import AddOrder from './pages/page-add-order';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-import Profile from './pages/page-profile';
-import Order from './pages/page-order';
-=======
->>>>>>> 7d9f73d7a68de79050819f32618094b866ebfae8
- 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -20,13 +15,8 @@ root.render(
         <Route path='/' element={<Login/>}/>
         <Route path='/add-product' element={<AddProduct/>}/>
         <Route path='/edit-product/:id' element={<EditProduct/>}/>
-<<<<<<< HEAD
-        <Route path='products' element={<Products/>}/>
-        <Route path='/profile/:id' element={<Profile/>}/>
-        <Route path='/page-order' element={<Order/>}/>
-=======
         <Route path='/products' element={<Products/>}/>
->>>>>>> 7d9f73d7a68de79050819f32618094b866ebfae8
+        <Route path='/add-order' element={<AddOrder/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
