479be5ab4b862da0429d36b8e5866915d0a5789a
CarlosDuarte <ra52208844840@acaonsfatima.org.br>
2024-11-22 09:16:23 -0300

    Delete

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,8 @@ import Login from './pages/page-login';
 import AddProduct from './pages/page-add-product';
 import EditProduct from './pages/page-edit-product';
 import Products from './pages/page-product';
-import AddOrder from './pages/page-add-order';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Order from './pages/page-orders';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,8 +17,6 @@ root.render(
         <Route path='/add-product' element={<AddProduct/>}/>
         <Route path='/edit-product/:id' element={<EditProduct/>}/>
         <Route path='/products' element={<Products/>}/>
-        <Route path='/add-order' element={<AddOrder/>}/>
-        <Route path='/orders' element={<Order/>}/>
       </Routes>
     </BrowserRouter>
     <Toaster/>
--- src/pages/page-add-product/index.jsx
+++ src/pages/page-add-product/index.jsx
@@ -1,5 +1,5 @@
 import './index.scss';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Sidebar from '../../components/comp-sidebar';
 
 import axios from 'axios';
